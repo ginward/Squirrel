057be95e95581952231fe7941e1821181401b335
班章 <localhost>
2016-12-20 14:21:17 -0500

    added the test case

--- src/main/java/SquirrelTranscriber.java
+++ src/main/java/SquirrelTranscriber.java
@@ -1,9 +1,36 @@
 /**
  * Created by jinhuawang on 20/12/2016.
- * License MIT 
+ * License MIT
  */
-public class SquirrelTranscriber {
-    public static void main(String[] args){
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+public class SquirrelTranscriber {
+    public static void main(String[] args) throws IOException {
+        Configuration configuration = new Configuration();
+
+        configuration
+                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration
+                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration
+                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
+                configuration);
+        InputStream stream = new FileInputStream(new File("/Users/jinhuawang/Squirrel/src/main/java/005 copy.wav"));
+
+        recognizer.startRecognition(stream);
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        }
+        recognizer.stopRecognition();
     }
 }
